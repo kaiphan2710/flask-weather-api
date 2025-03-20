a563a4c55b9b45e7e78c650c0b8feef5ded77792
Kai <kaiphan2710@gmail.com>
2025-03-20 21:23:58 +1000

    Fix API route

--- app.py
+++ app.py
@@ -26,5 +26,6 @@ def get_weather():
         "weather": data["weather"][0]["description"]
     })
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)  # Flask chạy trên port 5000
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
+
